513d63ff9b0478c5f5b310737164c9793fe776f2
AnasIDSD <anaskhaldouny@gmail.com>
2023-01-11 15:15:18 +0100

    Start Up

---
+++ connection/connection.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const connection = async () => {
+    try {
+        const con = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        })
+        console.log(`Mongo connected successfuly: ${con.connection.host}`)
+    } catch (err) {
+        console.log('Database connection error: ', err.message)
+    }
+}
+
+module.exports = connection
